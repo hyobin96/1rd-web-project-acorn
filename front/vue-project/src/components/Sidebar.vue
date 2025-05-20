8864888e994e1f8ec05358e221f29f14d7044a4a
yujinoh <yonggari_gari@naver.com>
2025-05-20 17:56:29 +0900

    메인페이지 만드는 중

---
+++ front/vue-project/src/components/Sidebar.vue
@@ -0,0 +1,37 @@
+<template>
+    <nav class="sidebar-container">
+        <div class="sidebar">
+            <div class="account">
+                <img src=""/>
+                <span>사용자 설정 닉네임</span>
+            </div>
+            <div class="nav-content">
+                <div class="create-playlist"><span>플레이리스트 만들기</span></div>
+                <div class="dashboard">
+                    <span>대시보드</span>
+                </div>
+                <div class="event-page">
+                    <span>이벤트</span>
+                </div>
+                <div class="shopping-page">
+                    <span>쇼핑</span>
+                </div>
+                <div class="challenge-age">
+                    <span>소셜</span>
+                </div>
+            </div>
+            <div class="user-menu">
+                <div class="mypage"><span>마이페이지</span></div>
+                <div class="logout"><span>로그아웃</span></div>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ front/vue-project/src/views/MainView.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h2>메인페이지 만드는 중</h2>
+        <RouterView/>
+        <nav class="navbar">
+            <div class="logo-container"><a class="logo-icon">로고 이미지</a></div>
+            <div class="account"><span>y</span></div>
+        </nav>
+        <div class="motivation-banner">
+            <span>응원하는 말 넣는 곳~~~</span>
+        </div>
+        <main>
+            <div class="favorite-playlist-section">
+                <div class="favorite-playlist"><span class="favorite-playlist-name">사용자 설정 영상 제목1</span></div>
+                <div class="favorite-playlist"><span class="favorite-playlist-name">사용자 설정 영상 제목1</span></div>
+                <div class="favorite-playlist"><span class="favorite-playlist-name">사용자 설정 영상 제목1</span></div>
+            </div>
+            <div class="playlist-list">
+            </div>
+        </main>
+
+    </div> 
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+ .navbar{
+    border: 1px solid red;
+ }
+</style>
