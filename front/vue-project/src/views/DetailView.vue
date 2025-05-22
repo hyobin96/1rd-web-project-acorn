58c3eaebc3c0dd293af94a5314ce139ba1f2f72e
yujinoh <yonggari_gari@naver.com>
2025-05-22 15:15:58 +0900

    상세 페이지 70% 정도 틀 완성

--- front/vue-project/src/views/DetailView.vue
+++ front/vue-project/src/views/DetailView.vue
@@ -9,9 +9,9 @@
             <iframe src="https://www.youtube.com/watch?v=rlh76p4T6qw&list=WL" allow="autoplay" allowfullscreen> <!--일단 오토플레이로 설정-->
             </iframe>
         </div>
-        <div class="video-title">감성을 깨우는 커피 향과 고요한 피아노 선율</div>
+        <div class="video-title">[허리스트레칭] 허리 통증 완화에 좋은 스트레칭 모음👌🏻｜ 💥허리 아프신분들 주목💥</div>
         <div class="memo-container">
-            <span class="memo-count">메모 n개</span>
+            <div class="memo-count"><span>메모 n개</span></div>
             <div class="memo">
                 <div class="creation-box">
                     <input type="text" @keyup.enter="registMemo" v-model="memo">
@@ -25,13 +25,15 @@
         </div>
         <div class="playlist-detail-sidebar">
             <div class="sidebar-timer">
-                타이머
+                <span>Timer</span>
+                <div class="timer"></div>
             </div>
             <div class="sidebar-calender">
-                캘린더
+                <div class="calender">캘린더 자리</div>
             </div>
             <div class="sidebar-next-videos">
-                다음 영상
+                <span>Next</span>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="18" height="18"><path d="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6 .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4 191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160 80 80 0 1 0 0 160z"/></svg>
             </div>
         </div>
         </div>
@@ -70,7 +72,7 @@ input, button {
   background: none;
   font: inherit;
   color: inherit;
-  appearance: none; /* 브라우저 기본 스타일 제거 */
+  appearance: none;
   -webkit-appearance: none;
   -moz-appearance: none;
 }
@@ -96,21 +98,24 @@ input, button {
 }
 
 .video-title{
+    width: 900px;
     height: 28px;
     font-size: 20px;
     /* border: 1px solid rebeccapurple; */
     margin-top: 12px;
     margin-bottom: 24px;
     font-weight: 700;
+    word-break: break-all;
+}
+
+.memo-count{
+    padding-top: 12px;
+    /* border: 1px solid salmon; */
 }
 
 .playlist-detail-sidebar{
     width: 300px;
-    border: 1px solid yellow;
-}
-
-.memo-container{
-    /* border: 1px solid darkblue; */
+    /* border: 1px solid yellow; */
 }
 
 .creation-box > input{
@@ -132,8 +137,9 @@ input, button {
     /* font-size: 10px; */
     font-family: Roboto, Arial, sans-serif;
     border-radius: 15px;
-    width: 38px;
-    height: 25px;
+    width: 40px;
+    height: 28px;
+    line-height: 28px;
     text-align: center;
 }
 
@@ -143,9 +149,48 @@ input, button {
     background-color: rgb(182, 179, 179); /*버튼 색 미정 */
     /* font-size: 10px; */
     font-family: Roboto, Arial, sans-serif;
-    border-radius: 15px;
-    width: 38px;
-    height: 25px;
+    border-radius: 13px;
+    width: 40px;
+    height: 28px;
+    line-height: 28px;
     text-align: center;
+}
+
+.sidebar-timer{
+    height: 76px;
+    border: 1px solid darkblue;
+    padding-top: 8px;
+    padding-left: 10px;
+}
+
+.sidebar-timer > .timer{
+    border: 1px solid rgb(159, 127, 97);
+    height: 30px;
+    margin-top: 3px;
+    margin-right: 10px;
+    font-size: 16px;
+}
+
+.sidebar-calender{
+    height: 272px;
+    border: 1px solid darkcyan;
+}
+
+.sidebar-next-videos {
+    margin-top: 8px;
+    margin-left: 10px;
+    display: flex;
+    justify-content: space-between;
+}
+
+.sidebar-next-videos > svg{
+    height: 30px;
+    margin-right: 10px;
+}
+
+.sidebar-next-videos > span{
+    border: 1px solid darkgoldenrod;
+    font-size: 16px;
+    height: 30px;
 }
 </style>
