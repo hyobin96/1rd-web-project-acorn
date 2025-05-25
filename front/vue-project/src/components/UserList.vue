ef6a4d9c03facb8c4442a9dd021c9c852b6df28f
yujinoh <peacefu1me@naver.com>
2025-05-26 01:50:38 +0900

    정지 회원 정보 불러오기 완료

--- front/vue-project/src/components/UserList.vue
+++ front/vue-project/src/components/UserList.vue
@@ -7,8 +7,8 @@
                     <th>ID</th>
                     <th>email</th>
                     <th>닉네임</th>
-                    <th>가입일</th>
-                    <th>정지</th>
+                    <th>가입일시</th>
+                    <th>상태</th>
                 </tr>
             </thead>
             <tbody>
@@ -18,7 +18,7 @@
                     <td>{{ user.email }}</td>
                     <td>{{ user.nickname }}</td>
                     <td>{{ formatDate(user.createdAt) }}</td>
-                    <td><button @click="editEvent(user)">정지</button></td>
+                    <td><button @click="editEvent(user)">{{ user.isDeleted === true || user.isDeleted === 'true' || user.isDeleted === 1 || user.isDeleted === '1' ? '정지 회원' : '정상 회원' }}</button></td>
                 </tr>
             </tbody>
         </table>
@@ -26,33 +26,53 @@
 </template>
 
 <script setup>
-import {ref, onMounted} from 'vue'
+import { ref, onMounted } from 'vue'
 import api from '@/api/axios'
 
 const users = ref([])
 
 /* 날짜 형식에 맞게 고치기 */
-function formatDate(dateStr){
-    if(!dateStr) return ''
+function formatDate(dateStr) {
+    if (!dateStr) return ''
     const [year, month, day] = dateStr.split('-')
     return `${year.slice(2)}/${month}/${day}`
 }
 
 /* 사용자 정보 로딩하기 */
 async function loadUserInfo() {
-    try{
-        const {data} = await api.get("/users")
-        users.value = data
-    }catch(err){
-        console.log(err.response?.data || err.message);
-        
-    }
-    
+  try {
+    const { data } = await api.get("/users");
+  console.log("서버에서 받아온 원본 데이터:", data); 
+    users.value = data.map(user => ({
+      ...user,
+      isDeleted: user.isDeleted === true || user.isDeleted === 'true' || user.isDeleted === 1 || user.isDeleted === '1'
+    }));
+
+  } catch (err) {
+    console.log(err.response?.data || err.message);
+  }
 }
 
 /* 사용자 정지하기 */
-function editEvent(user) {
-    console.log('정지할 유저:', user);
+async function editEvent(user) {
+    if (user.isDeleted) {
+        alert('이미 정지된 사용자입니다.');
+        return;
+    }
+
+    if (!confirm('정지 처리 하시겠습니까?')) return;
+    try {
+        await api.put(`/users/${user.id}`)
+        console.log(`유저는 왜 정지가 계속 되는가?` ,user.isDeleted);
+        
+        alert('정지 처리 완료!')
+        await loadUserInfo(); 
+
+    } catch (err) {
+        console.log(err.response?.data || err.message);
+        alert('정지 처리 실패!');
+
+    }
 }
 
 onMounted(() => {
@@ -61,5 +81,15 @@ onMounted(() => {
 </script>
 
 <style scoped>
+table {
+    border-collapse: collapse;
+    /*테두리가 겹치는 것 방지 */
+}
 
+th,
+td {
+    border: 1px solid rgba(255, 106, 0, 0.534);
+    text-align: center;
+    min-width: 200px;
+}
 </style>
