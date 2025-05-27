ab77fd8debf5df196887238fe9d19b0bfc027f0f
hyobin <dnlem5415@gmail.com>
2025-05-27 09:20:22 +0900

    모달 취소기능 구현

--- front/vue-project/src/components/Sidebar.vue
+++ front/vue-project/src/components/Sidebar.vue
@@ -18,7 +18,7 @@
                             d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 144L48 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l144 0 0 144c0 17.7 14.3 32 32 32s32-14.3 32-32l0-144 144 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-144 0 0-144z" />
                     </svg><span @click="showModal = true" style="cursor: pointer;">플레이리스트 만들기</span>
                     <ModalView v-if="showModal" @close="showModal = false">
-                        <CreatePlaylistView />
+                        <CreatePlaylistView @close = "showModal = false"/>
                     </ModalView>
                 </div>
 
